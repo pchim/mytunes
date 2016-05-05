eed0838668de9912e5020378daa7c0b5195d9044
pchim <pchim24@gmail.com>
2016-05-04 17:43:13 -0700

    Start song models and song queue model implementations

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,26 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    console.log(this);
+    this.on('add', function() {
+      if (this.length === 1) { 
+        this.playFirst(this.at(0));
+      } else {
+        this.at(0).on('change:ended', this.playFirst);
+      }
+    }, this);
+
+
+
+  },
+
+  playFirst: function(song) {
+    //console.log('called in playfirst', song.trigger('ended'));
+
+    song.on('ended', function() {
+      console.log('ended');
+      this.shift();
+    });
   }
 
 });
